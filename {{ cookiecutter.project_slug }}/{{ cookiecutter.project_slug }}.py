9a153c2e59e081ac447f5ffe5719fdf711f03ac1
ArcHound <miloslav.homer@kiwi.com>
2022-11-18 11:26:35 +0100

    feat: init commit

---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import click
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+
+@click.command()
+@click.option(
+    "--log-level",
+    default="WARNING",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+    show_default=True,
+    help="Set logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)"
+    )
+def main(log_level):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    log_levels = {"DEBUG":logging.DEBUG, "INFO":logging.INFO, "WARNING":logging.WARNING, "ERROR":logging.ERROR, "CRITICAL":logging.CRITICAL}
+    logging.getLogger().setLevel(log_levels[log_level])
+    return 0
+
+if __name__ == "__main__":
+    main()
